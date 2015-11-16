e15b6b4138219e0984f32f4c55386fa0500a023b
MaestracciDamien <damien.maestracci@etu.univ-nantes.fr>
2015-11-16 10:34:20 +0100

    bj

---
+++ src/NormalCard.cpp
@@ -0,0 +1,23 @@
+#include "NormalCard.hpp"
+
+
+NormalCard::NormalCard(int id,bool h, int pos, int pow, Action * a) :Card(id), _hero(h), _position(pos), _power(pow), _action(a){
+
+}
+
+
+void NormalCard::action(){
+	_action->action();
+}
+
+bool NormalCard::isHero(){
+	return _hero;
+}
+
+int NormalCard::getPosition(){
+	return _position;
+}
+
+int NormalCard::getPower(){
+	return _power;
+}
---
+++ src/NormalCard.hpp
@@ -0,0 +1,17 @@
+#include "Card.hpp"
+#include "Action.hpp"
+class NormalCard : public Card{
+	private:
+		
+		bool _hero;
+		int _position;
+		int _power;
+		Action *_action;
+
+	public:
+		NormalCard(int id, bool h, int pos, int pow, Action *a);
+		void action();
+		bool isHero();
+		int getPosition();
+		int getPower();
+};
